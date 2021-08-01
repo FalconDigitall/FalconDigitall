b978d73cc0b2d5768e5ebca6d5176065d578b7c3
FalconDigitall <88264516+FalconDigitall@users.noreply.github.com>
2021-08-01 11:32:30 +0500

    Create MainIdea.c

---
+++ MainIdea.c
@@ -0,0 +1,15 @@
+/**Hi,
+i know this has been a very basic and dont fullfill the exact criteria of call for code challenge but the moment I got heard for the 
+first time the theme for Call for code 2021 for ** Sustainable production and Zero hunger then i thought below idea should have been an ideal fit for that.
+@Judges you may please consider accordingly.
+
+In Muslim communities across the world . practciing Muslims have to offer offer prayers routinely 5 times a day followed by water abulution. That Water is purely clean
+and mostly drinking water used only for washing hands, face and feets and then water is simply drained and practically wasted in most part of the land especially in South East Asia
+of Pakistan , India and BanglaDesh.
+
+What we suggest that we can catch and contained that water in same mosque vicnity and recycled and 
+  can be used for small area plantation mmostly on roof top of the mosques
+  can be filtered and used as water in the tankers to roam in the city for planatation and construction works.
+  
+  */
+  
